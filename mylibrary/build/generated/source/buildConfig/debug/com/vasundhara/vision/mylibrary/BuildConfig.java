5cfe7bd5407b48015720bd41fb06d94177f3fe1b
akshay3.vasundhara@gmail.com <vasundhara>
2023-02-15 17:58:17 +0530

    1.0.0

---
+++ mylibrary/build/generated/source/buildConfig/debug/com/vasundhara/vision/mylibrary/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.vasundhara.vision.mylibrary;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.vasundhara.vision.mylibrary";
+  public static final String BUILD_TYPE = "debug";
+}
